f14bf4169497f1672818e02ef27fd1a45bf579c6
Carlo Kop <56932525+carlokop@users.noreply.github.com>
2025-02-28 14:42:23 +0100

    sdk

--- app/src/main/java/nl/ou/buddyapp/MainActivity.kt
+++ app/src/main/java/nl/ou/buddyapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package nl.ou.buddyapp
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,6 +13,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import nl.ou.buddyapp.ui.theme.BuddyAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,6 +36,26 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val db = Firebase.firestore
+    // Create a new user with a first and last name
+    val user = hashMapOf(
+        "first" to "Ada",
+        "last" to "Lovelace",
+        "born" to 1815
+    )
+
+// Add a new document with a generated ID
+    db.collection("users")
+        .add(user)
+        .addOnSuccessListener { documentReference ->
+            Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+        }
+        .addOnFailureListener { e ->
+            Log.w(TAG, "Error adding document", e)
+        }
+
+
+
     Text(
         text = "Hello $name!",
         modifier = modifier
